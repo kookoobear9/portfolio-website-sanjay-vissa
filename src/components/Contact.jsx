0d1720716c52aa6451b63f190422ef3a05140d09
kookoobear9 <sanjayvissa9@gmail.com>
2024-04-13 23:17:21 -0400

    bd

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -77,11 +77,10 @@ const Contact = () => {
         
         {/* <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="sanjayvissa" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/sanjayvissa?trk=profile-badge"></a></div> */}
         
-        <div class="linkedin-badge">
-    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="sanjayvissa" data-version="v1">
-        <a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/sanjayvissa?trk=profile-badge"></a>
-    </div>
-</div>
+       
+        <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="sanjayvissa" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/sanjayvissa?trk=profile-badge"></a></div>
+              
+
 
         
         <p className={styles.sectionSubText}>
