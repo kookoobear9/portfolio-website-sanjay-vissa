e8f3d3f0b601c3fe352c6ac2c632466e62918a4a
Sanjay <sanjayvissa9@gmail.com>
2025-02-04 15:26:21 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-white'>Sanjay</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            An industrial engineer, product manager,  <br className='sm:block hidden' />
-            self-taught developer, UI/UX designer,  <br className='sm:block hidden' />
+            An industrial engineer, product manager, consultant <br className='sm:block hidden' />
+            self-taught UI/UX designer and developer,  <br className='sm:block hidden' />
             tech enthusiast, and artist. <br className='sm:block hidden' /> <br className='sm:block hidden' />
             I am an aspiring jack of all trades with a passion for learning. <br className='sm:block hidden' />
             I believe in making life simple, efficient and stress-free, <br className='sm:block hidden' />
