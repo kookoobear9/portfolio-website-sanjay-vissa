d6df41c969d1928de89e548a32bdc693d43ac2f1
Sanjay <sanjayvissa9@gmail.com>
2025-06-02 20:36:06 -0400

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -42,7 +42,7 @@ const ExperienceCard = ({ experience }) => {
         </p>
       </div>
 
-      {/* <ul className='mt-5 list-disc ml-5 space-y-2'>
+      { <ul className='mt-5 list-disc ml-5 space-y-2'>
         {experience.points.map((point, index) => (
           <li
             key={`experience-point-${index}`}
@@ -51,7 +51,7 @@ const ExperienceCard = ({ experience }) => {
             {point}
           </li>
         ))}
-      </ul> */}
+      </ul> }
     </VerticalTimelineElement>
   );
 };
